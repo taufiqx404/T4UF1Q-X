00fd8f60a787af30ebc03aab4b49d3f6eee0eb4d
taufiqx404 <59577411+taufiqx404@users.noreply.github.com>
2020-08-20 21:33:06 +0800

    Update and rename README.md to anonymous.sh

---
+++ anonymous.sh
@@ -0,0 +1,198 @@
+#usr/bin/bash
+clear
+bi='\033[34;1m' #biru
+ij='\033[32;1m' #ijo
+pr='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+echo
+echo
+echo
+figlet -f slant  "TAUFIQ X"|lolcat
+sleep 4
+echo $ku" TUNGGU SEBENTAR LAGI MASUK KE TOOLS"
+sleep 1
+echo $me" LOADING"
+sleep 1
+echo $me"█▒▒▒▒▒▒▒▒▒"
+echo $me"    10%   "
+sleep 1
+echo $ku"███▒▒▒▒▒▒▒"
+echo $ku"    30%   "
+sleep 1
+echo $i"█████▒▒▒▒▒"
+echo $i"    50%   "
+sleep 1
+echo $bi"███████▒▒▒"
+echo $bi"   100%   "
+sleep 1
+echo $cy"██████████"
+sleep 1
+echo $pr" LOGIN BERHASIL"
+sleep 1
+clear
+echo $me"===================================="
+echo $me" WELCOME TO MY TOOLS TOL"
+echo $me"===================================="
+sleep 1
+echo $pu" AUTHOR :"TAUFIQ X"$pu"
+sleep 1
+echo $pu" TEAM   :"RED DEVIL OFF"$pu"
+sleep 1
+echo $pu" GITHUB :"taufiqx/404"$pu"
+sleep 1
+echo $pu"===================================="
+echo
+echo $cy" SILAHKAN PILIH MENU TOOLS NYA"
+sleep 1
+echo $me"─────█─▄▀█──█▀▄─█──"
+sleep 1
+echo $me"────▐▌──────────▐▌──"
+sleep 1
+echo $pu"────█▌▀▄──▄▄──▄▀▐█──"
+sleep 1
+echo $pu"───▐██──▀▀──▀▀──██▌──"
+sleep 1
+echo $pu"──▄████▄──▐▌──▄████▄─"
+sleep 1
+echo $me" ANONYMOUS"$pu" INDONESIA"
+echo
+echo $me"|"$me" 1"$me".  DDOS ATTACK "
+sleep 1
+echo $ku"|"$ku" 2"$ku".  HACK CCTV "
+sleep 1
+echo $i"|"$i" 3"$i".  TROJANS "
+sleep 1
+echo $cy"|"$cy" 4"$cy".  PHISING "
+sleep 1
+echo $pr"|"$pr" 5"$pr".  DARK FB "
+sleep 1
+echo $bi"|"$bi" 6"$bi".  AUTO DEFACE "
+sleep 1
+echo $ku"|"$ku" 7"$ku".  KATOOLIN "
+sleep 1
+echo $me"|"$me" 8"$me".  VIRUS CREATE "
+sleep 1
+echo $cy"|"$cy" 9"$cy".  INSTALL BAHAN DLU YA TOL"
+sleep 1
+echo $i"|"$i " 10"$i ".  KELUAR DARI PROGRAM AING TOL"
+echo $me"---"$pur"["$me"RED DEVIL OFF"$me"]"$pu"------"$pu"["$pu""pilih tools""$cy"]"
+echo $ku"|"
+read -p" ------>" pil
+
+if [ $pil = 1 ]
+then
+clear
+figlet -f slant " W A I T "|lolcat
+sleep1
+git clone https://github.com/MrTamfanX/MrDdos
+cd MrDdos
+chmod +x*
+sh Tamfan-Ddos.sh
+fi
+
+if [ $pil = 2 ]
+then
+clear
+figlet -f slant " W A I T "|lolcat
+sleep1
+git clone https://github.com/kancotdiq/ipcs
+cd ipcs
+python2 ipcs.py
+fi
+
+if [ $pil = 3 ]
+then
+clear
+figlet -f slant " W A I T "|lolcat
+sleep 1
+git clone https://github.com/justahackers/perusak
+cd perusak
+python2 perusak.py
+fi
+
+if [ $pil = 4 ]
+then
+clear
+figlet -f slant " W A I T "|lolcat
+sleep 1
+git clone https://github.com/evait-security/weeman.git
+cd weeman
+python2 weeman.py
+
+if [ $pil = 5 ]
+then
+clear
+figlet -f slant " W A I T "|lolcat
+sleep 1
+git clone https://github.com/taufiqx404/blackhattersakiti
+cd blackhattersakiti
+python2 dkfb.py
+fi
+
+if [ $pil = 6 ]
+then
+clear
+figlet -f slant " W A I T "|lolcat
+sleep 1
+git clone https://github.com/404rgr/webdav77
+cd webdav77
+bash install.sh
+fi
+
+if [ $pil = 7 ]
+then
+clear
+figlet -f slant " W A I T "|lolcat
+sleep 1
+git clone https://github.com/LionSec/katoolin.git
+cd katoolin
+python2 katoolin.py
+fi
+
+if [ $pil = 8 ]
+then
+clear
+figlet -f slant " W A I T "|lolcat
+sleep 1
+git clone https://github.com/saydog/Virus-droid-creator
+cd Virus-droid-creator
+python2 dog.py
+fi
+
+if [ $pil = 5 ]
+then
+clear
+apt update && apt upgrade
+apt install python2
+pip2 install urllib3 chardet certifi idna requests
+pkg install git
+pip2 install mechanize
+pkg install curl
+pkg install ruby
+pkg install gem
+gem install lolcat
+pkg install git
+pkg install php
+pkg install ruby cowsay toilet figlet
+pkg install neofetch
+pkg install nano
+figlet -f slant "B E R H A S I L"|lolcat
+fi
+
+if [ $pil = 6 ]
+then
+clear
+figlet -f slant "E X I T"|lolcat
+sleep 2
+echo $ku" T H A N K S T O L"
+sleep2
+echo $cy"OKE TOL"
+sleep 2
+echo $i"RED DEVIL OFF"
+sleep 2
+echo $pur"MAKASIH UDAH MAMPIR TOL"
+exit
+fi
